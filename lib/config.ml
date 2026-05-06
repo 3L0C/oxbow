70c1a717a677d9d4a0ede2fa3bbf55307afa71b3
3L0C <141392286+3L0C@users.noreply.github.com>
2026-05-06 02:24:50 -0700

    tweak: default config gaps

--- lib/config.ml
+++ lib/config.ml
@@ -8,30 +8,27 @@ type t =
   ; mutable focus_follows_pointer : bool
   }
 
+let default_layout_params =
+  Layout_params.
+    { mfact = 0.55; nmaster = 1; gaps_inner = 10; gaps_outer = 20; stack = Stack_even }
+;;
+
+let default_borders =
+  Border_config.
+    { width = 4
+    ; focused_color = 0xFFFFFFl
+    ; unfocused_color = 0xFFFFFFl
+    ; urgent_color = 0xFFFFFFl
+    }
+;;
+
 let create_tag_data ~(entry : Layout_entry.t) : Tag_data.t =
-  { layout_params =
-      { mfact = 0.55; nmaster = 1; gaps_inner = 0; gaps_outer = 0; stack = Stack_even }
-  ; layout_entry = entry
-  }
+  { layout_params = default_layout_params; layout_entry = entry }
 ;;
 
 let default (entry : Layout_entry.t) : t =
-  { default_tag_config =
-      { layout_params =
-          { mfact = 0.55
-          ; nmaster = 1
-          ; gaps_inner = 0
-          ; gaps_outer = 0
-          ; stack = Stack_even
-          }
-      ; layout_entry = entry
-      }
-  ; borders =
-      { width = 4
-      ; focused_color = 0xFFFFFFl
-      ; unfocused_color = 0xFFFFFFl
-      ; urgent_color = 0xFFFFFFl
-      }
+  { default_tag_config = { layout_params = default_layout_params; layout_entry = entry }
+  ; borders = default_borders
   ; modkey = Rwm.River_seat_v1.Modifiers.mod4
   ; rules = []
   ; focus_follows_pointer = true
