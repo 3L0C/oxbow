34652645f97f470f281fe42ba0d4c12886d2eb86
3L0C <141392286+3L0C@users.noreply.github.com>
2026-07-19 03:19:00 -0700

    add: rule to send windows to outputs

--- lib/ctl/cmd_rule_add.ml
+++ lib/ctl/cmd_rule_add.ml
@@ -1,4 +1,7 @@
 let name = "add"
 let doc = "Add a window rule"
-let cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_tag.add_cmd ]
-let bind_cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_tag.add_bind_cmd ]
+let cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_send.add_cmd; Cmd_rule_tag.add_cmd ]
+
+let bind_cmd =
+  Ctl_cli.group ~name ~doc [ Cmd_rule_send.add_bind_cmd; Cmd_rule_tag.add_bind_cmd ]
+;;
--- lib/ctl/cmd_rule_remove.ml
+++ lib/ctl/cmd_rule_remove.ml
@@ -1,4 +1,7 @@
 let name = "remove"
 let doc = "Remove a window rule"
-let cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_tag.remove_cmd ]
-let bind_cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_tag.remove_bind_cmd ]
+let cmd = Ctl_cli.group ~name ~doc [ Cmd_rule_send.remove_cmd; Cmd_rule_tag.remove_cmd ]
+
+let bind_cmd =
+  Ctl_cli.group ~name ~doc [ Cmd_rule_send.remove_bind_cmd; Cmd_rule_tag.remove_bind_cmd ]
+;;
