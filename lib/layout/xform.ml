39df65c3278c7a8dd94c21a7a9f901ea151ae368
3L0C <141392286+3L0C@users.noreply.github.com>
2026-07-20 02:22:17 -0700

    fix: incorrect swap logic in xform

--- lib/layout/xform.ml
+++ lib/layout/xform.ml
@@ -9,7 +9,9 @@ let pre (dir : Direction.Spatial.t) (area : int Rect.t) =
 let post (dir : Direction.Spatial.t) ~(area : int Rect.t) (rect : int Rect.t) =
   let mirror_x (r : int Rect.t) = { r with x = area.x + area.w - (r.x - area.x) - r.w } in
   let mirror_y (r : int Rect.t) = { r with y = area.y + area.h - (r.y - area.y) - r.h } in
-  let swap (r : int Rect.t) = { r with x = area.x + r.y; y = area.y + r.x } in
+  let swap (r : int Rect.t) =
+    Rect.{ x = area.x + r.y; y = area.y + r.x; w = r.h; h = r.w }
+  in
   match dir with
   | Left -> rect
   | Right -> mirror_x rect
