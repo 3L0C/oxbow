90125f24e1c31b86e7ba294ce0cbfcbc70a8c6c4
3L0C <141392286+3L0C@users.noreply.github.com>
2026-08-27 13:20:54 -0700

    refactor: remove nix/xkbcommon.nix
    Packaged upstream in nixpkgs-unstable

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,11 +1,3 @@
-final: prev:
-let
-  ocamlPackages = prev.ocamlPackages.overrideScope (
-    ofinal: _: {
-      xkbcommon = ofinal.callPackage ./xkbcommon.nix { };
-    }
-  );
-in
-{
-  oxbow = final.callPackage ../default.nix { inherit ocamlPackages; };
+final: _: {
+  oxbow = final.callPackage ../default.nix { };
 }
