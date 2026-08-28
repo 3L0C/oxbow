1c09c34b70db36ca6b95c74f5b07eac86454e8ae
3L0C <141392286+3L0C@users.noreply.github.com>
2026-08-27 17:52:34 -0700

    fix: sync oxctl man page

--- checks.nix
+++ checks.nix
@@ -3,7 +3,8 @@
   pkgs,
   ocamlformat,
   ...
-}: {
+}:
+{
   pre-commit-check = inputs.git-hooks.lib.${pkgs.stdenv.hostPlatform.system}.run {
     src = ./.;
 
@@ -15,18 +16,19 @@
         name = "ocamlformat";
         description = "Format OCaml files with ocamlformat";
         entry = "${ocamlformat}/bin/ocamlformat --inplace";
-        types = ["ocaml"];
+        types = [ "ocaml" ];
       };
 
       check-merge-conflicts = {
         enable = true;
-        excludes = ["^protocols/.*\\.ml$"];
+        excludes = [ "^protocols/.*\\.ml$" ];
       };
       end-of-file-fixer = {
         enable = true;
         excludes = [
           "^protocols/.*\\.ml$"
           "^test/.*\\.expected$"
+          "^bin/.*\\.1$"
         ];
       };
       trim-trailing-whitespace = {
@@ -34,6 +36,7 @@
         excludes = [
           "^protocols/.*\\.ml$"
           "^test/.*\\.expected$"
+          "^bin/.*\\.1$"
         ];
       };
     };
